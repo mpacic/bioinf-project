ca768cfd8ab703b768a8eff5381956e4b5817f03
ivanakulis <ivanakulis123@gmail.com>
2023-05-31 14:44:22 +0200

    Fixed formatting

--- src/SCCGC.cpp
+++ src/SCCGC.cpp
@@ -1,19 +1,18 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <filesystem>
 #include <string>
 #include <unordered_map>
 #include <vector>
 using namespace std;
 
 class SCCGC {
-
  public:
-  SCCGC(
-    std::string referenceGenomePath,
-    std::string inputFilePath,
-    std::string outputDirPath
-  ) : referenceGenomePath(referenceGenomePath), inputFilePath(inputFilePath), outputDirPath(outputDirPath) {};
+  SCCGC(std::string referenceGenomePath, std::string inputFilePath,
+        std::string outputDirPath)
+      : referenceGenomePath(referenceGenomePath),
+        inputFilePath(inputFilePath),
+        outputDirPath(outputDirPath){};
   ~SCCGC(){};
   void run();
   void buildGlobalHashTable(const string reference, int kmer_length);
@@ -27,33 +26,37 @@ class SCCGC {
   std::string referenceSeq;
   int kmer_size;
   static const int maxchar = 67108864;
-  static const int ght_maxlen = 268435456; 
+  static const int ght_maxlen = 268435456;
   std::vector<int> kmer_location;
   std::vector<int> next_kmer;
 };
 
 int main(int argc, char **argv) {
   // check number of arguments
-  if(argc < 4) {
-    std::cout << "Usage: " << argv[0] << " <reference genome file> <input file> <output_directory>" << std::endl;
+  if (argc < 4) {
+    std::cout << "Usage: " << argv[0]
+              << " <reference genome file> <input file> <output_directory>"
+              << std::endl;
     return 1;
   }
 
   // check reference genome file exists
-  if(!filesystem::exists(argv[1])) {
-    std::cout << "Error: Reference genome file does not exist: " << argv[1] << std::endl;
+  if (!filesystem::exists(argv[1])) {
+    std::cout << "Error: Reference genome file does not exist: " << argv[1]
+              << std::endl;
     return 1;
   }
 
   // check input file exists
-  if(!filesystem::exists(argv[2])) {
+  if (!filesystem::exists(argv[2])) {
     std::cout << "Error: Input file does not exist: " << argv[2] << std::endl;
     return 1;
   }
 
   // check output directory exists
-  if(!filesystem::exists(argv[3])) {
-    std::cout << "Error: Output directory does not exist: " << argv[3] << std::endl;
+  if (!filesystem::exists(argv[3])) {
+    std::cout << "Error: Output directory does not exist: " << argv[3]
+              << std::endl;
     return 1;
   }
 
@@ -66,7 +69,7 @@ int main(int argc, char **argv) {
 std::string parseReferenceGenome(std::string referenceGenomePath) {
   ifstream referenceGenomeFile(referenceGenomePath);
   // check file opened successfully
-  if(!referenceGenomeFile.is_open()) {
+  if (!referenceGenomeFile.is_open()) {
     std::cout << "Error: Failed to open reference genome file" << std::endl;
   }
 
@@ -75,8 +78,8 @@ std::string parseReferenceGenome(std::string referenceGenomePath) {
   // skip first line
   getline(referenceGenomeFile, line);
 
-  while(getline(referenceGenomeFile, line)) {
-    for(int i = 0; i < line.length(); i++) {
+  while (getline(referenceGenomeFile, line)) {
+    for (int i = 0; i < line.length(); i++) {
       char c = toupper(line[i]);
       if (c != 'N') {
         referenceGenome += c;
@@ -89,7 +92,7 @@ std::string parseReferenceGenome(std::string referenceGenomePath) {
 std::string readTargetGenome(std::string targetGenomePath) {
   ifstream targetGenomeFile(targetGenomePath);
   // check file opened successfully
-  if(!targetGenomeFile.is_open()) {
+  if (!targetGenomeFile.is_open()) {
     std::cout << "Error: Failed to open target genome file" << std::endl;
   }
 
@@ -98,8 +101,8 @@ std::string readTargetGenome(std::string targetGenomePath) {
   // skip first line
   getline(targetGenomeFile, line);
 
-  while(getline(targetGenomeFile, line)) {
-    for(int i = 0; i < line.length(); i++) {
+  while (getline(targetGenomeFile, line)) {
+    for (int i = 0; i < line.length(); i++) {
       targetGenome += line[i];
     }
   }
@@ -113,16 +116,17 @@ void SCCGC::run() {
 
   // open files
   ifstream inputFile(this->inputFilePath);
-  
-  if(!inputFile.is_open()) {
+
+  if (!inputFile.is_open()) {
     std::cout << "Error: Failed to open input file" << std::endl;
   }
 
   // tempfile
   string tempfilePath = outputDirPath + "/intermediate.txt";
   ofstream tempfile(tempfilePath);
-  if(!tempfile.is_open()) {
-    std::cout << "Error: Failed to open intermediate file:" << tempfilePath << std::endl;
+  if (!tempfile.is_open()) {
+    std::cout << "Error: Failed to open intermediate file:" << tempfilePath
+              << std::endl;
   }
 
   // parse reference genome file
@@ -144,7 +148,7 @@ void SCCGC::buildGlobalHashTable(const string reference, int kmer_length) {
   // allocate hash table
   kmer_location = std::vector<int>(ght_maxlen);
   next_kmer = std::vector<int>(iters);
-  
+
   // set all hash table entries to default value
   std::fill(kmer_location.begin(), kmer_location.end(), -1);
 
@@ -161,33 +165,33 @@ void SCCGC::buildGlobalHashTable(const string reference, int kmer_length) {
 
 void SCCGC::buildLocalHashTable(const string reference, int kmer_length) {
   // build local hash table
-
 }
 
-std::vector<std::pair<int, int>> SCCGC::getLowercasePositions(const string input) {
+std::vector<std::pair<int, int>> SCCGC::getLowercasePositions(
+    const string input) {
   std::vector<std::pair<int, int>> positions;
   bool multiple = false;
   int start = 0;
-  for(int i = 0; i < input.length(); i++) {
-    if(std::islower(input[i])) {
-      if(multiple) {
+  for (int i = 0; i < input.length(); i++) {
+    if (std::islower(input[i])) {
+      if (multiple) {
         continue;
       } else {
         start = i;
         multiple = true;
       }
     } else {
-      if(multiple) {
+      if (multiple) {
         positions.push_back(std::make_pair(start, i));
         multiple = false;
       }
       start = 0;
     }
   }
-  
+
   // check if the last subsequence is all lowercase
-  if(multiple) {
-    positions.push_back(std::make_pair(start, input.length()-1));
+  if (multiple) {
+    positions.push_back(std::make_pair(start, input.length() - 1));
   }
   return positions;
 }
